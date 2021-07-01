a5408be8c70ffd8a97cc94687f1152aae84144b9
FR-68 <84712601+FR-68@users.noreply.github.com>
2021-07-01 06:49:45 +0530

    Solution

---
+++ swap.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void ssort(int arr[],int l)
+{
+    for(int i=0;i<l;i++)
+    { swap_max(arr,l,i);
+    }
+}
+
+void swap_max(int arr[],int l,int n)
+{
+    int j,t;
+    int temp;
+
+    for(j=t=n;j<l;j++)
+    {
+        if(arr[j] > arr[k])
+        {   t = j;
+        }
+    }
+    temp = arr[n];
+    arr[n] = arr[k];
+    arr[k] = temp;
+}
